2e4e12c2abbaa6b2298eaee2a2a48a4a4ca3b7a3
annami <amm3@wp.pl>
2017-12-17 14:36:03 +0100

    initial commit for sudoku exercise

---
+++ sudoku/src/amilacz/java2/sudoku/SudokuApp.java
@@ -0,0 +1,30 @@
+package amilacz.java2.sudoku;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class SudokuApp {
+	public static void main(String[] args) {
+
+		try {
+			Workbook workbook = WorkbookFactory.create(new File("D:\\Documents\\podyplomowka\\cwiczenia\\java2-2\\cwiczenia\\sudoku\\sudoku.xlsx"));
+			SudokuBoardChecker sbc =new SudokuBoardChecker(workbook);
+			boolean checkIfOK = sbc.verifyBoardStructure(0);
+			System.out.println("board has right structure: "+checkIfOK);
+			if (checkIfOK) {
+				if(sbc.verifyBoardCorrectness(0)) {
+					System.out.println("board has right values");
+				}else {
+					System.out.println("some values are repeated");
+				}
+			}
+			
+		} catch (EncryptedDocumentException | InvalidFormatException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
